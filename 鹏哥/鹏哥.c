9efaa5be5865b2313561c5393440634781bf836c
王渊 <2682220169@qq.com>
2022-08-19 12:33:15 +0800

    更新

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -3,11 +3,118 @@
 #include<string.h>
 int main()
 {
-	int arr[10] = { 0 };
-	printf("%d\n", sizeof(arr)/sizeof(arr[0]));
-	printf("%d\n", sizeof(int));
+	//int arr[10] = { 5,6,7,8,9 };
+	//printf("%d\n", arr[4]);
+	int a = 10;
+	int b = 20;
+	int sum = Add(a, b);
+	printf("%d\n", sum);
 	return 0;
 }
+int Add(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
+}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = 0;
+//	max = (a > b ? a : b);
+//	printf("%d", max);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 5;
+//	int c = a || b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = (int)3.14;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	int b = --a;
+//	printf("a=%d b=%d\n", a, b);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = ~a;
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	int arr[] = { 1,2,3,4,5,6, };
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof a);
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr) / sizeof(arr[0]));
+//	return 0;
+//}
+
+
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int max = Max(num1, num2);
+//	printf("max=%d\n", max);
+//	//if (num1 > num2)
+//	//	printf("较大值是：%d\n", num1);
+//	//else
+//	//	printf("较大值是：%d\n", num2); 
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[] = { 'a','b','c' };
+//	printf("%d", strlen(arr));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%d\n", sizeof(arr)/sizeof(arr[0]));
+//	printf("%d\n", sizeof(int));
+//	return 0;
+//}
 
 
 //int main()
