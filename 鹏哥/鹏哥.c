1d5e7e384c1cb5f96d4607a4b1ac15fdfec8c7b1
王渊 <2682220169@qq.com>
2022-08-22 07:23:43 +0800

    更新 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -1,30 +1,61 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
-struct Book
-{
-	char name[100];
-	short price;
-};
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 1)
+//		if (b == 2)
+//			printf("hehe\n");
+//		else
+//			printf("haha\n");
+//
+//	return 0;
+//}
 
-int main()
-{
-	struct Book b1 = { "c语言程序设计",55 };
-	struct Book* pb = &b1;
-	strcpy(b1.name, "C++");
-	//b1.name = "C++";
-	b1.price = 15;
-	printf("%s\n", pb->name);
-	printf("%d\n", pb->price);
-	printf("%s\n", (*pb).name);
-	printf("%d\n", (*pb).price);
-	//printf("书名:%s\n", b1.name);
-	//printf("价格:%d元\n", b1.price);
-	//b1.price = 15;
-	//printf("修改后的价格：%d\n", b1.price);
 
-	return 0;
-}
+//int main()
+//{
+//	int age = 20;
+//	if (age < 18)
+//		printf("未成年\n");
+//	else if (age >= 18 && age < 28)
+//		printf("青年\n");
+//	else if (age >= 50 && age < 90)
+//		printf("老年\n");
+//	else
+//		printf("老不死\n");
+//	//else
+//	//	printf("成年\n");
+//	return 0;
+//}
+
+
+//struct Book
+//{
+//	char name[100];
+//	short price;
+//};
+//
+//int main()
+//{
+//	struct Book b1 = { "c语言程序设计",55 };
+//	struct Book* pb = &b1;
+//	strcpy(b1.name, "C++");
+//	//b1.name = "C++";
+//	b1.price = 15;
+//	printf("%s\n", pb->name);
+//	printf("%d\n", pb->price);
+//	printf("%s\n", (*pb).name);
+//	printf("%d\n", (*pb).price);
+//	//printf("书名:%s\n", b1.name);
+//	//printf("价格:%d元\n", b1.price);
+//	//b1.price = 15;
+//	//printf("修改后的价格：%d\n", b1.price);
+//
+//	return 0;
+//}
 
 
 //int main()
