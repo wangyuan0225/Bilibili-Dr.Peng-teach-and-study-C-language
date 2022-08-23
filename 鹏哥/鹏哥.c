fe2fc051b46abfc8227295d327266cdc1ae4bd81
王渊 <2682220169@qq.com>
2022-08-23 21:57:00 +0800

    更新 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -1,15 +1,201 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		putchar(ch);
+	}
+	//int ch = getchar();
+	//putchar(ch);
+	//printf("%c\n", ch);
+	return 0;
+}
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		i++;
+//		if (5 == i)
+//			continue;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		printf("%d ", i);
+//		i++;
+//	}
+//
+//	//while (1)
+//	//	printf("hehe\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 1;
+//	int m = 2;
+//	switch (n)
+//	{
+//	case 1:
+//		m++;//1,3
+//	case 2:
+//		n++;//2,3
+//	case 3:
+//		switch (n)
+//		{
+//		case 1:
+//			n++;
+//		case 2:
+//			m++;//2,4
+//			n++;//3,4
+//			break;
+//		}
+//	case 4:m++;//3,5
+//		break;
+//	default:
+//		break;
+//	}
+//	printf("%d %d\n", n, m);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int day = 0;
+//	int n = 1;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		if (1 == n)
+//			printf("hehe\n");
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("休息日\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch(day)
+//	{
+//	case 1:
+//		printf("星期1\n");
+//		break;
+//
+//	case 2:
+//		printf("星期2\n");
+//		break;
+//
+//	case 3:
+//		printf("Wednesday\n");
+//		break;
+//
+//	case 4:
+//		printf("Thursday\n");
+//		break;
+//
+//	case 5:
+//		printf("Friday\n");
+//		break;
+//
+//	case 6:
+//		printf("Saturday\n");
+//		break;
+//
+//	case 7:
+//		printf("Sunday\n");
+//		break;
+//	}
+//	//if (1 == day)
+//	//	printf("星期1\n");
+//	//else if (2 == day)
+//	//	printf("星期2\n");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num = 1;
+//	while (num <= 100)
+//	{
+//		if (num < 100)
+//		{
+//			printf("%d ", num);
+//			num+=2;
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num = 4;
+//	if (5 == num) 
+//	{
+//		printf("hehe\n");
+//	}
+//
+//	return 0;
+//}
+
+
 //int main()
 //{
 //	int a = 0;
 //	int b = 2;
 //	if (a == 1)
+//	{
 //		if (b == 2)
 //			printf("hehe\n");
-//		else
-//			printf("haha\n");
+//	}
+//	else
+//		printf("haha\n");
 //
 //	return 0;
 //}
