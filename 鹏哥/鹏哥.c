24f74bf2e911ed87bdac847c841321914d3bdfc5
王渊 <2682220169@qq.com>
2022-08-25 23:56:03 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -3,17 +3,103 @@
 #include<string.h>
 int main()
 {
-	int i = 1;
-	do
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int left = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int right = sz - 1;
+	while ()
 	{
-		if (i == 5)
-			continue;
-		printf("%d ", i);
-		i++;
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			printf("找到了，下标是:%d\n", mid);
+			break;
+		}
 	}
-	while (i <= 10);
 	return 0;
 }
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 17;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (k == arr[i])
+//		{
+//			printf("找到了，下标是：%d\n", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 4; n++)
+//	{
+//		ret = ret * n;
+//		sum = sum + ret;
+//
+//	}
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 3; n++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (5 == i)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	while (i <= 10);
+//	return 0;
+//}
 
 
 //int main()
