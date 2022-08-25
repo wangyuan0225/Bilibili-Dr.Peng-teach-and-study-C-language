48dd35cb82537fee034450ceaa6d34cfbd887b8f
王渊 <2682220169@qq.com>
2022-08-25 18:51:11 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -3,14 +3,75 @@
 #include<string.h>
 int main()
 {
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int i = 0;
-	for (i = 0; i < 10; i++)
+	int i = 1;
+	do
 	{
-		printf("%d", arr[i]);
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+		i++;
 	}
+	while (i <= 10);
 	return 0;
 }
+
+
+//int main()
+//{
+//	int i = 0;
+//	int k = 0;
+//	for (i = 0, k = 0; k = 0; i++, k++)
+//		k++;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int x, y;
+//	for (x = 0, y = 0; x < 2 && y < 5; ++x, y++)
+//	{
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 10; i++)
+//	{
+//		for (; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	for (;;)
+//	{
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	return 0;
+//}
 
 
 //int main()
@@ -28,7 +89,7 @@ int main()
 
 //int main()
 //{
-//	int i = 0;
+//	int i ;
 //	for (i = 1; i <= 10; i++)
 //	{
 //		if (5 == i)
