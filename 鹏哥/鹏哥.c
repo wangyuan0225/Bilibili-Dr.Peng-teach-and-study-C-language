9e1c62030b3b25e883acf0f894fcf3401df6c4f1
王渊 <2682220169@qq.com>
2022-08-26 21:50:48 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -1,6 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
+#include<windows.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = { 0 };
+//	for(i = 0; i < 3; i++)
+//	{
+//		printf("请输入密码:>");
+//		scanf("%s", password);
+//		if (strcmp(password, "123456") == 0)
+//		{
+//			printf("登陆成功\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("密码错误\n");
+//		}
+//	}
+//	if (3 == i)
+//		printf("三次密码均错误，退出程序\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr1[] = "welcome to bit!!!!!!";
+//	char arr2[] = "                    ";
+//	int left = 0;
+//	//int right = sizeof(arr1)/sizeof(arr1[0]-2);
+//	int right = strlen(arr1)-1;
+//
+//	while (left<=right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		Sleep(1000);
+//		system("cls");
+//		left++;
+//		right--;
+//		
+//	}
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+
 //int main()
 //{
 //	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
