996fdffc70c43d12e2141a740abe617680c0d2b3
王渊 <2682220169@qq.com>
2022-08-27 12:25:16 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -6,6 +6,106 @@
 
 //int main()
 //{
+//	int a, b, c, d, m, i;
+//	scanf("%d %d", &a, &b);
+//	for (i = 0; ; i++)
+//	{
+//		if (a > b)
+//		{
+//			c = a % b;
+//			a = b;
+//			b = c;
+//			d = a;
+//		}
+//		else if (a < b)
+//		{
+//			c = b % a;
+//			b = a;
+//			a = c;
+//			d = b;
+//		}
+//		if (a == b)
+//		{
+//			d = a;
+//			break;
+//		}
+//		if (c == 0)  break;
+//	}
+//	printf("%d", d);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int m, n, r;
+//	scanf("%d%d", &m, &n);
+//	while (r = m % n)
+//	{
+//		m = n;
+//		n = r;
+//	}
+//	printf("%d\n", n);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i < 100; i++)
+//	{
+//		if (i % 3 == 0)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (i = 5)
+//			printf("%d", i);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
 //	int i = 0;
 //	char password[20] = { 0 };
 //	for(i = 0; i < 3; i++)
