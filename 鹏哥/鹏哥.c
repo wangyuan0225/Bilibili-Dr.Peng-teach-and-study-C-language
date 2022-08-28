8663fa285bce35fd2f7d3e92490153f796a1133f
王渊 <2682220169@qq.com>
2022-08-28 23:24:57 +0800

    蒸的C，测试了live share功能

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -5,6 +5,32 @@
 #include<stdlib.h>
 #include<math.h>
 
+int main()
+{
+	
+	return 0;
+}
+
+
+int main()
+{
+	int a = 0, b = 0;
+	for (a = 1, b = 1; a <= 100; a++)
+	{
+		if (b >= 20)
+			break;
+		if (b % 3 == 1)
+		{
+			b += 3;
+			continue;
+
+		}b -= 5;
+	}
+	printf("%d\n", a);
+	return 0;
+}
+
+
 //int main()
 //{
 //	int i = 0;
