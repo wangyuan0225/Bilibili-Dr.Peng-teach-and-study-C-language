18412a5e93f7f0059a05defc85f98cef1f18473c
王渊 <2682220169@qq.com>
2022-08-31 20:07:46 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -8,14 +8,73 @@
 int main()
 {
 	int i = 0;
-	double sum = 0.0;
-	for (i = 1; i <= 100; i++)
+	for(i=1;i<=9;i++)
 	{
-		sum += 1.0 / i;
+		int j = 1;
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%-2d ", i, j, i * j);
+
+		}
+		printf("\n");
+			
 	}
-	printf("%lf\n", sum);
 	return 0;
 }
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 0;
+//	for (i = 1; i < 10; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	double sum1 = 0;
+//	double sum2 = 0;
+//	
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 1)
+//		{
+//			sum1 += 1.0 / i;
+//		}
+//		else
+//		{
+//			sum2 += 1.0 / i;
+//		}
+//	}
+//	printf("%lf\n", sum1 - sum2);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag*1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
 
 
 //int main()
