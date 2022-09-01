cd61ba92704d919a3f0b58ed7081d9e58a22cec6
王渊 <2682220169@qq.com>
2022-09-01 22:59:06 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -4,48 +4,80 @@
 #include<windows.h>
 #include<stdlib.h>
 #include<math.h>
-
-void game()
-{
-	int ret = 0;
-	srand();
-	ret = rand();
-	printf("%d\n", ret);
-
-}
-
-void menu()
-{
-	printf("********************************\n");
-	printf("****    1.play    0.exit    ****\n");
-	printf("********************************\n");
-}
+#include<time.h>
 
 int main()
-
 {
-	int input = 0;
-	do
-	{
-		menu();
-		printf("请选择>:");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("退出游戏\n");
-			break;
-		default:
-			printf("选择错误\n");
-			break;
-		}
-	} while (input);
+	printf("hello bit\n");
+	goto again;
+	printf("你好\n");
+again:
+	printf("hehe\n");
 	return 0;
-
 }
+
+
+//void game()
+//{
+//	int ret = 0;
+//	int guess = 0;
+//	ret = rand() % 100 + 1;
+//	while (1)
+//	{
+//		
+//		printf("请猜数字:>");
+//		scanf("%d", &guess);
+//		if (guess > ret)
+//		{
+//			printf("猜大了\n");
+//		}
+//		else if (guess < ret)
+//		{
+//			printf("猜小了\n");
+//		}
+//		else
+//		{
+//			printf("恭喜你，猜对了\n");
+//			break;
+//
+//		}
+//	}
+//
+//}
+//
+//void menu()
+//{
+//	printf("********************************\n");
+//	printf("****    1.play    0.exit    ****\n");
+//	printf("********************************\n");
+//}
+//
+//int main()
+//
+//{
+//	int input = 0;
+//	srand((unsigned int)time(NULL));
+//	do
+//	{
+//		menu();
+//		printf("请选择>:");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("选择错误\n");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//
+//}
 
 
 //int main()
