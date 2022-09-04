a35dc5710b8a2c10be235910f11058ed0ed12f08
王渊 <2682220169@qq.com>
2022-09-04 14:07:44 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -6,29 +6,39 @@
 #include<math.h>
 #include<time.h>
 
-//int main()
-//{
-//
-//	return 0;
-//}
+int Add(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
 
-
+}
 int main()
 {
-	char input[20] = { 0 };
-	system("shutdown -s -t 60");
-	while (1)
-	{
-		printf("请注意，你的电脑在一分钟内关机，如果输入：我是猪，就取消关机\n请输入》:");
-		scanf("%s", input);
-		if (strcmp(input, "我是猪") == 0)
-		{
-			system("shutdown -a");
-			break;
-		}
-	}
+	int a = 10;
+	int b = 20;
+	int sum = Add(a, b);
+	printf("%d\n", sum);
 	return 0;
 }
+
+
+//int main()
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");
+//	while (1)
+//	{
+//		printf("请注意，你的电脑在一分钟内关机，如果输入：我是猪，就取消关机\n请输入》:");
+//		scanf("%s", input);
+//		if (strcmp(input, "我是猪") == 0)
+//		{
+//			system("shutdown -a");
+//			break;
+//		}
+//	}
+//	return 0;
+//}
 
 
 //int main()
