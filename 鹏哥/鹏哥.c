a25fbc10eb8d5130b8057ad6a78242d074cf2f85
王渊 <2682220169@qq.com>
2022-09-11 13:17:49 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -6,30 +6,100 @@
 #include<math.h>
 #include<time.h>
 
-int is_prime(int n)
-{
-	int j = 0;
-	for (j = 2; j <= sqrt(n); j++)
-	{
-		if (n % j == 0)
-		{
-			return 0;
-		}
-	}
-	return 1;
-}
-
 int main()
 {
-	int i = 0;
-	for (i = 100; i <= 200; i++)
-	{
-		if (is_prime(i) == 1)
-			printf("%d ", i);
-	}
+	int num = 0;
+	Add();
 	return 0;
-
 }
+
+
+//binary_search(int arr[], int k, int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//}
+//
+//int main()
+// {
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr, k, sz);
+//	if (ret == 1)
+//	{
+//		printf("找不到指定的数字\n");
+//	}
+//	else
+//	{
+//		printf("找到了，下标是：%d\n", ret);
+//	}
+//	return 0;
+//}
+
+
+//int is_leap_year(int y)
+//{
+//	if ((y % 4==0 && y % 100 != 0) || (y % 400 == 0))
+//		return 1;
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (is_leap_year(year) == 1)
+//		{
+//			printf("%d ", year);
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int is_prime(int n)
+//{
+//	int j = 0;
+//	for (j = 2; j <= sqrt(n); j++)
+//	{
+//		if (n % j == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		if (is_prime(i) == 1)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//
+//}
 
  
 //不能完成任务
