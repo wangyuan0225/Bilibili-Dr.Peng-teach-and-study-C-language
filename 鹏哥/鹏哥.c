c07b2d9ecfa9d83cffbe030a2b00a9d662309c1d
王渊 <2682220169@qq.com>
2022-09-15 14:49:47 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -6,39 +6,109 @@
 #include<math.h>
 #include<time.h>
 
-int my_strlen(char* str)
+int Fib(int n)
 {
-	if (*str != '\0')
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while (n >= 3)
 	{
-		return 1 + my_strlen(str + 1);
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
 	}
-	else
-	{
-		return 0;
-	}
+	return c;
 }
 
-//int my_strlen(char* str)
+//int count = 0;
+//int Fib(int n)
 //{
-//	int count = 0;
-//	while (*str != '\0')
+//	if (n == 3)
 //	{
 //		count++;
-//		str++;
 //	}
-//	return count;
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
 //}
 
 int main()
 {
-	char arr[] = "bit";
-	//int len = strlen(arr);
-	//printf("%d\n", len);
-	int len = my_strlen(arr);
-	printf("len=%d\n", len);
-
+	int n = 0;
+	scanf("%d", &n);
+	int ret = Fib(n);
+	printf("ret=%d\n", ret);
+	//printf("count=%d\n", count);
 	return 0;
 }
+
+
+
+//int Fac1(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;
+//}
+//
+//int Fac2(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac2(n-1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = 0;
+//	ret = Fac2(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//int my_strlen2(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return 1 + my_strlen2(str + 1);
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//
+////int my_strlen(char* str)
+////{
+////	int count = 0;
+////	while (*str != '\0')
+////	{
+////		count++;
+////		str++;
+////	}
+////	return count;
+////}
+//
+//int main()
+//{
+//	char arr[] = "bit";
+//	//int len = strlen(arr);
+//	//printf("%d\n", len);
+//	int len = my_strlen2(arr);
+//	printf("len=%d\n", len);
+//	
+//	return 0;
+//}
 
 
 //void print(int n)
