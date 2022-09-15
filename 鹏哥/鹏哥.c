8ca4b0629fc73f8180c1bb5e5cf219adc540477f
王渊 <2682220169@qq.com>
2022-09-15 23:55:54 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -6,44 +6,66 @@
 #include<math.h>
 #include<time.h>
 
-int Fib(int n)
-{
-	int a = 1;
-	int b = 1;
-	int c = 1;
-	while (n >= 3)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		n--;
-	}
-	return c;
-}
-
-//int count = 0;
-//int Fib(int n)
-//{
-//	if (n == 3)
-//	{
-//		count++;
-//	}
-//	if (n <= 2)
-//		return 1;
-//	else
-//		return Fib(n - 1) + Fib(n - 2);
-//}
-
 int main()
 {
-	int n = 0;
-	scanf("%d", &n);
-	int ret = Fib(n);
-	printf("ret=%d\n", ret);
-	//printf("count=%d\n", count);
+	int arr[10];
+	char arr2[5];
 	return 0;
 }
 
+
+//void test(int n)
+//{
+//	if (n < 10000)
+//	{
+//		test(n + 1);
+//	}
+//}
+//
+//int main()
+//{
+//	test(1);
+//	return 0;
+//}
+
+
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n >= 3)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//
+////int count = 0;
+////int Fib(int n)
+////{
+////	if (n == 3)
+////	{
+////		count++;
+////	}
+////	if (n <= 2)
+////		return 1;
+////	else
+////		return Fib(n - 1) + Fib(n - 2);
+////}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Fib(n);
+//	printf("ret=%d\n", ret);
+//	//printf("count=%d\n", count);
+//	return 0;
+//}
 
 
 //int Fac1(int n)
