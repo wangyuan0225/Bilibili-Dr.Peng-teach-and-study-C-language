28cf161449f45e21b0e0eb5724e22fdf698f3f92
王渊 <2682220169@qq.com>
2022-09-30 23:11:40 +0800

    Update 鹏哥.c

--- 鹏哥/鹏哥.c
+++ 鹏哥/鹏哥.c
@@ -8,11 +8,44 @@
 
 int main()
 {
-	char arr[] = "abcdef";
+	int arr[3][4] = { {1,2,3},{4,5} };
+	char ch[5][6];
 
 	return 0;
-
 }
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%p\n", &arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	//char arr[] = "abcdef";
+//	//printf("%c\n",arr[3]);
+//	//int i = 0;
+//	//int len = strlen(arr);
+//	//for (i = 0; i < len; i++)
+//	//{
+//	//	printf("%c ", arr[i]);
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
 
 
 //int main()
